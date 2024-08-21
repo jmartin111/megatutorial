a66505b3bb289e813ac1aba9ba43da5575c65b1e
Jeff Martin <chess.logic@gmail.com>
2024-08-21 13:50:59 -0700

    Ch. 4 Complete Pt. 2 - add flask shell context

--- microblog.py
+++ microblog.py
@@ -1,1 +1,17 @@
-from app import blog
+#! .venv/bin/python
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import blog, db
+from app.models import User, Post
+
+@blog.shell_context_processor
+def make_shell_context():
+    return {
+        'sa': sa,
+        'so': so,
+        'db': db,
+        'User': User,
+        'Post': Post
+    }
