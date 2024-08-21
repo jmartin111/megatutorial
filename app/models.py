4fbaf0e9696281e78f3c39f91381072b6188f7ce
Jeff Martin <chess.logic@gmail.com>
2024-08-20 23:14:59 -0700

    Ch. 4 - db migrate 'posts table'

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 #! .venv/bin/python
 
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sql
 import sqlalchemy.orm as orm
@@ -14,5 +15,18 @@ class User(db.Model):
                                                index=True, unique=True)
     pw_hash: orm.Mapped[Optional[str]] = orm.mapped_column(sql.String(256))
 
+    posts: orm.WriteOnlyMapped['Post'] = orm.relationship(back_populates='author')
+
     def __repr__(self) -> str:
         return f'User: {self.username}; Email: {self.email}'
+    
+
+class Post(db.Model):
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    body: orm.Mapped[str] = orm.mapped_column(sql.String(144))
+    timestamp: orm.Mapped[datetime] = orm.mapped_column(index=True,
+                                        default=lambda: datetime.now(timezone.utc))
+    user_id: orm.Mapped[int] = orm.mapped_column(sql.ForeignKey(User.id),
+                                                 index=True)
+    
+    author: orm.Mapped[User] = orm.relationship(back_populates='posts')
