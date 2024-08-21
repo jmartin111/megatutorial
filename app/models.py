b9356c46c74917920337a2cad3cb2dbfc8c26f25
Jeff Martin <chess.logic@gmail.com>
2024-08-21 13:32:42 -0700

    Ch. 4 Complete - DB Migrations

--- app/models.py
+++ app/models.py
@@ -12,8 +12,7 @@ class User(db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
