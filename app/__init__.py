f8b741f59e6dda24580bf67f727308cf4ec77f6a
Jeff Martin <chess.logic@gmail.com>
2024-08-20 21:12:20 -0700

    Complete Ch. 3 - Web Forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 #! .venv/bin/python3
 
 from flask import Flask
+from config import BlogConfig
 
 blog = Flask(__name__)
+blog.config.from_object(BlogConfig)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,44 @@
 #! .venv/bin/python
 
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import blog
+from app.forms import LoginForm, DummyForm
 
 @blog.route('/')
 @blog.route('/index')
 def index():
     meta = {
-        "username": "jeff",
-        "title": "index"
+        'username': 'jeff',
+        'title': 'index'
         }
     posts = [
         {
-            "author": {"username": "jeff"},
-            "title": "one",
-            "body": "ipsum delorium, libyans have plutonium!!"
+            'author': {'username': 'jeff'},
+            'title': 'one',
+            'body': 'ipsum delorium, libyans have plutonium!!'
         },
         {
-            "author": {"username": "some fool"},
-            "title": "two",
-            "body": "jesus saves your everlasting soul"
+            'author': {'username': 'some fool'},
+            'title': 'two',
+            'body': 'jesus saves your everlasting soul'
         },
         {
-            "author": {"username": "jeff"},
-            "title": "three",
-            "body": "no he does not because that is just silly"
+            'author': {'username': 'jeff'},
+            'title': 'three',
+            'body': 'no he does not because that is just silly'
         },
     ]
-    return render_template("index.html",
-                           title=meta["title"],
-                           user=meta["username"], 
+    return render_template('index.html',
+                           title=meta['title'],
+                           user=meta['username'], 
                            posts=posts)
+
+
+@blog.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login required for {form.username.data}, '
+                      f'remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='SIgn In', form=form)
