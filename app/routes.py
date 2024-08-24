6108b7d948899fee29e5ab9124a236e9b71e0217
Jeff Martin <chess.logic@gmail.com>
2024-08-23 22:26:43 -0700

    Ch 5 Complete - User profile/avatar/edit implemented

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,25 @@
 #! .venv/bin/python
 
-from curses.ascii import US
+from crypt import methods
+from datetime import datetime, timezone
+from flask_migrate import current
 import sqlalchemy as sa
 
 from flask import render_template, flash, redirect, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
 from app import blog, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 from urllib.parse import urlsplit
+
+@blog.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
 
 @blog.route('/')
 @blog.route('/index')
@@ -40,7 +49,8 @@ def index():
     return render_template('index.html',
                            title=meta['title'],
                            user=meta['username'], 
-                           posts=posts)
+                           posts=posts,
+                           )
 
 
 @blog.route('/register', methods=['GET', 'POST'])
@@ -78,7 +88,7 @@ def login():
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='SIgn In', form=form)
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @blog.route('/user/<username>')
@@ -91,7 +101,24 @@ def user(username):
         {'author': user, 'body': 'test post 2'}
     ]
 
-    return render_template('user.html', user=user, posts=posts)
+    return render_template('user.html', title='Profile', user=user, posts=posts)
+
+
+@blog.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
 
 
 @blog.route('/logout')
